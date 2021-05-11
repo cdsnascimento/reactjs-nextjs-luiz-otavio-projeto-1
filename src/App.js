0a0cadff2e19520e08ac3681c5bea639496183a8
Cristiano Nascimento <cdsnascimento@hotmail.com>
2021-05-10 23:54:44 -0300

    12-Componentes de Classe com estado (StateFull)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+      name: 'Cristiano da Silva Nascimento',
+      counter: 0
+    };
+
+  handlePClick = () => {
+    this.setState({name: 'Cristiano Nascimento'});
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter + 1});
+  }
+
+  render() {
+    
+    /*
+    //Forma primariação
+    const name = this.state.name
+    */
+
+    //Forma desestruturada (mais usada)
+    const { name, counter }= this.state;
+    const msg = `Olá mundo! ${counter}`;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {msg}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {name}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
+
+
+/* 
+
+function App() {
+
+}
+ */
 export default App;
