3e8545feab90b76dcd1b20d15d1176571695870f
Cristiano Nascimento <cdsnascimento@hotmail.com>
2021-05-19 01:00:26 -0300

    29-Migrando para React Hooks

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,100 @@
 
 import './styles.css';
 
-import { Component } from 'react';
+import { Component, useCallback, useEffect } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts';
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
+import { useState } from 'react';
 
-export class Home extends Component {
+export const Home = () => {
+
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
+  
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? 
+  allPosts.filter( post => {
+      return post.title.toLowerCase().includes(
+        searchValue.toLowerCase()
+      );
+  }) 
+  : posts;  
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {   
+    const postAndPhotos = await loadPosts();
+
+    setPosts(postAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postAndPhotos);
+  }, []);
+  
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+    
+const loadMorePosts = () => {
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+  
+  }
+  
+  const handleChange = (e) => {
+      const { value } = e.target;
+
+      setSearchValue(value);
+  }
+  
+
+  
+
+      return (
+      <section className="container">
+        <div className="search-container">
+
+          {!!searchValue && (
+            <h1>Seaarch Value: {searchValue}</h1>
+          )}
+
+          <TextInput searchValue={searchValue} handleChange={handleChange} />
+
+        </div>
+
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts} />
+        )}
+
+        {filteredPosts.length === 0 && (
+          <p>Não exitem mais posts</p>
+        )}
+
+        <div className="button-container">
+          {!searchValue && (
+            <Button
+              text="Load More Posts"
+              onClick={loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
+        </div>
+
+      </section>
+
+    );
+
+}
+
+
+export class Home2 extends Component {
   state = {
     posts: [],
     allPosts: [],
